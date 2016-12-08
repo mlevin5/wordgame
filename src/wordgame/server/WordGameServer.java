9de821136162aabff5a03bb0346e259cc7a06eff
Megan Levin <mlevin@mit.edu>
2016-12-08 18:14:49 -0500

    fixed another small typo

--- src/wordgame/server/WordGameServer.java
+++ src/wordgame/server/WordGameServer.java
@@ -1,6 +1,3 @@
-/* Copyright (c) 2007-2016 MIT 6.005 course staff, all rights reserved.
- * Redistribution of original or derived work requires permission of course staff.
- */
 package wordgame.server;
 
 import java.io.*;
